e97a8b7058f89c9ad528cc69605c5b9418a8d4ef
Unknown <SWright441@west-mec.org>
2018-08-08 18:21:22 -0700

    added functions and created event listeners
    added function to update days select list for months with 28, 30, and 31 days, created a function to see if custom message is checked, created function to create event listeners for delivery month, year, and message box

--- snoot.js
+++ snoot.js
@@ -37,27 +37,88 @@ function setUpDays(){
 // function to update the days select list
 function updateDays() {
     var deliveryDay = document.getElementById("delivDy");
-    var dates = deliveryDay.getElementsByTagName("options");
+    var dates = deliveryDay.getElementsByTagName("option");
     var deliveryMonth = document.getElementById("delivMo");
     var deliveryYear= document.getElementById("delivYr");
     var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
     while(dates[28]) {
         deliveryDay.removeChild(dates[28]);
     }
-    if ( deliveryMonth.selectedIndex === -1) {
-        deliveryMonth.selectedIndex = 0;
+    if (deliveryYear.selectedIndex === -1) {
+        deliveryYear.selectedIndex = 0;
     }
-    //if february and 2020  - leap year twentyNine
+    // if february and 2020  - leap year twentyNine
+    if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    }
 
     // else 30 day month - Thirty
+    else if(
+    selectedMonth === "4" 
+    || selectedMonth === "6" 
+    || selectedMonth === "9" 
+    || selectedMonth === "11") {
+        deliveryDay.appendChild(thirty.cloneNode(true));
+    }
 
-    //else 31 day month - thirtyOne
+    // else 31 day month - thirtyOne
+    else if (
+    selectedMonth === "1" 
+    || selectedMonth === "3" 
+    || selectedMonth === "5" 
+    || selectedMonth === "7" 
+    || selectedMonth === "8" 
+    || selectedMonth === "10" 
+    || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+    }
+
+}
+
+// function to see if custom message is checked
+function autoCheckCustom(){
+    var messageBox = document.getElementById("customText");
+    if (messageBox.value !== "" && messageBox.value !== messageBox.placeholder) {
+        // textarea actually has something in it
+        document.getElementById("custom").checked = "checked";
+    }
+    else {
+        // textarea has nothing
+        document.getElementById("custom").checked = "";
+    }
 }
 
 //functions to run on page load
 function setUpPage(){
     removeSelectDefaults();
+    setUpDays();
+    createEventListeners();
 }
+
+// function to create our event listeners
+function createEventListeners(){
+    var deliveryMonth = document.getElementById("delivMo");
+    if (deliveryMonth.addEventListener) {
+        deliveryMonth.addEventListener("change", updateDays, false);
+    } else if(deliveryMonth.attachEvent) {
+        deliveryMonth.attachEvent("onchange", updateDays);
+    }
+
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryYear.addEventListener) {
+        deliveryYear.addEventListener("change", updateDays, false);
+    } else if (deliveryYear.attachEvent) {
+        deliveryYear.attachEvent("onchange", updateDays);
+    }
+
+    var messageBox = document.getElementById("customText");
+    if (messageBox.addEventListener) {
+        messageBox.addEventListener("change", autoCheckCustom, false);
+    } else if (messageBox.attachEvent) {
+        messageBox.attachEvent("onchange", autoCheckCustom);
+    }
+}
+
 
 // enable load event handlers
 if (window.addEventListener) {
