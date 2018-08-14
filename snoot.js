8a72fd200540b848f1fbcd7e0e7385a4ed8c0290
Unknown <SWright441@west-mec.org>
2018-08-13 18:22:09 -0700

    update functions
    added missing code, debugged, validated select listeners,  updated validateDeliveryDate function

--- snoot.js
+++ snoot.js
@@ -8,25 +8,24 @@ Date:   08.06.18
 
 */
 
-"use strict";
 
+"use strict";
+// Variables that have Global/Module scope
 var twentyNine = document.createDocumentFragment();
 var thirty = document.createDocumentFragment();
 var thirtyOne = document.createDocumentFragment();
 var formValidity = false;
-
-// function to turn off select list defaults 
+// Function to turn off select list defaults
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
     for (var i = 0; i < emptyBoxes.length; i++) {
         emptyBoxes[i].selectedIndex = -1;
     }
 }
-
-// function to set up document fragments for days of month
-function setUpDays(){
-    //get the days option tags
-    var  dates = document.getElementById("delivDy").getElementsByTagName("option");
+// Function to set up document fragment for days of month
+function setUpDays() {
+    // Get the days select list in the delivery date portion of form
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
     twentyNine.appendChild(dates[28].cloneNode(true));
     thirty.appendChild(dates[28].cloneNode(true));
     thirty.appendChild(dates[29].cloneNode(true));
@@ -34,154 +33,253 @@ function setUpDays(){
     thirtyOne.appendChild(dates[29].cloneNode(true));
     thirtyOne.appendChild(dates[30].cloneNode(true));
 }
-
-// function to update the days select list
+// Function to update days selection list 
 function updateDays() {
     var deliveryDay = document.getElementById("delivDy");
     var dates = deliveryDay.getElementsByTagName("option");
     var deliveryMonth = document.getElementById("delivMo");
-    var deliveryYear= document.getElementById("delivYr");
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryMonth.selectedIndex === -1){
+        return;
+    }
     var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
-    while(dates[28]) {
+    while (dates[28]) {
         deliveryDay.removeChild(dates[28]);
     }
     if (deliveryYear.selectedIndex === -1) {
         deliveryYear.selectedIndex = 0;
     }
-    // if february and 2020  - leap year twentyNine
+    // If Feb and 2020 - leap year - twentyNine
     if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
         deliveryDay.appendChild(twentyNine.cloneNode(true));
     }
-
-    // else 30 day month - Thirty
-    else if(
-    selectedMonth === "4" 
-    || selectedMonth === "6" 
-    || selectedMonth === "9" 
-    || selectedMonth === "11") {
+    // Else 30 day month - thirty
+    else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
         deliveryDay.appendChild(thirty.cloneNode(true));
     }
-
-    // else 31 day month - thirtyOne
-    else if (
-    selectedMonth === "1" 
-    || selectedMonth === "3" 
-    || selectedMonth === "5" 
-    || selectedMonth === "7" 
-    || selectedMonth === "8" 
-    || selectedMonth === "10" 
-    || selectedMonth === "12") {
+    // Else 31 month - thirtyOne
+    else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" ||
+        selectedMonth === "7" || selectedMonth === "10" || selectedMonth === "12") {
         deliveryDay.appendChild(thirtyOne.cloneNode(true));
     }
-
 }
-
-// function to see if custom message is checked
+// Function to see if custom message is checked 
 function autoCheckCustom() {
     var messageBox = document.getElementById("customText");
     if (messageBox.value !== "" && messageBox.value !== messageBox.placeholder) {
-        // textarea actually has something in it
+        // Text area actually has something in it
         document.getElementById("custom").checked = "checked";
-    }
-    else {
-        // textarea has nothing
+    } else {
+        // Text area has nothing in it
         document.getElementById("custom").checked = "";
     }
 }
-
-// function to copy delivery to billing address
-function copyBillingAddress(){
+// Function to copy delivery to billing address
+function copyBillingAddress() {
     var billingInputElements = document.querySelectorAll("#billingAddress input");
     var deliveryInputElements = document.querySelectorAll("#deliveryAddress input");
-
-    // if checkbox checked - copy all fields
+    // If checkbox is checked, copy all fields
     if (document.getElementById("sameAddr").checked) {
-        for(var i = 0; i < billingInputElements.length; i++) {
-            deliveryInputElements[i + 1].value = billingInputElements[i].value;
+        for (var i = 0; i < billingInputElements.length; i++) {
+            deliveryInputElements[i + 1].value = billingInputElements[i].value
         }
-        document.querySelector("#deliveryAddress select").value = 
-        document.querySelector("#billingAddress select").value;
+        document.querySelector("#deliveryAddress select").value = document.querySelector("#billingAddress select").value;
     }
-    //else erase all fields
-    else{
-        for(var i = 0; i < billingInputElements.length; i++) {
-            deliveryInputElements[i + 1].value = "";
+    // Else, erase all fields
+    else {
+        for (var i = 0; i < billingInputElements.length; i++) {
+            deliveryInputElements[i + 1].value = ""
         }
         document.querySelector("#deliveryAddress select").selectedIndex = -1;
     }
+}
+// Function to validate entire form
+function validateForm(evt) {
+    if(evt.preventDefault) {
+       evt.preventDefault();
+    } else {
+       evt.returnValue = false;
+    }
+    
+    validateAddress("billingAddress");
+    
+    validateAddress("deliveryAddress");
 
+    validateDeliveryDate();
+    
+    if (formValidity === true) {
+        document.getElementById("errorText").innerHTML = "";
+        document.getElementById("errorText").style.display = "none";
+        document.getElementsByTagName("form")[0].submit();
+    } else {
+        document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0,0);
+    }
+}
+// Function to validate address
+function validateAddress(fieldsetId) {
+    var inputElements = document.querySelectorAll("#" + fieldsetId + " input");
+    var errorDiv = document.querySelectorAll("#" + fieldsetId + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var elementCount = inputElements.length;
+    var currentElement = null;
+    
+    try {
+        // Loop required input elements
+        for(var i = 0; i < elementCount; i++) {
+            currentElement = inputElements[i];
+            
+            // Test for blank
+            // alert(currentElement);
+
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255, 233, 233)";
+                fieldsetValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }  
+        }
+        //validate select listeners
+        currentElement = document.querySelectorAll("#" + fieldsetId + " select")[0];
+        //blank
+        if (currentElement.selectedIndex === -1) {
+            currentElement.style.border = "1 px solid red";
+            fieldsetValidity = false;
+        }
+        //valid
+        else{
+            currentElement.style.border = "white";
+        }
+
+        if (fieldsetValidity === false) {
+            if (fieldsetId === "billingAddress") {
+                throw "Please complete all Billing Address information";
+            }
+            else {
+                throw "Please complete all Delivery Address information";
+            }
+        }
+        else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+        
+    }
+    catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+    
 }
 
-//functions to run on page load
-function setUpPage(){
+//function to validate delivery date
+function validateDeliveryDate() {
+    var selectElements = document.querySelectorAll("#deliveryDate" + " select");
+    var errorDiv = document.querySelectorAll("#deliveryDate" + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var elementCount = selectElements.length;
+    var currentElement = null;
+ 
+    try {
+        // Loop required select elements
+        for(var i = 0; i < elementCount; i++) {
+            currentElement = selectElements[i];
+ 
+            // Test for blank
+            if (currentElement.selectedIndex === -1) {
+                currentElement.style.border = "1px solid red";
+                fieldsetValidity = false;
+            } else {
+                currentElement[i].style.border = "none";
+            }
+ 
+        }
+ 
+        // Validate select listeners
+ 
+        currentElement = document.querySelectorAll("#deliveryDate" + " select")[0];
+ 
+        // Blank
+ 
+        if (currentElement.selectedIndex === -1) {
+            currentElement.style.border = "1px solid red";
+            fieldsetValidity = false;
+        }
+        // Valid
+ 
+        else {
+            currentElement.style.border = "";
+        }
+ 
+        if (fieldsetValidity === false) {
+            throw "Please specify a delivery date";
+        }
+ 
+        else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+ 
+    }
+    catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+ }
+
+// Function to run on page load
+function setUpPage() {
     removeSelectDefaults();
     setUpDays();
     createEventListeners();
 }
-
-// function to validate entire form
-function validateForm(evt) {
-    alert("i am here");
-   if (evt.preventDefault) {
-       evt.preventDefault();
-   }else{
-    evt.returnValue = false;
-   }
-
-   if (formValidity === true) {
-       document.getElementById("errorText").innerHTML = "";
-       document.getElementById("errorText").style.display = "none";
-       document.getElementsByTagName("form")[0].submit();
-   }
-    else {
-    document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order.";
-    document.getElementById("errorText").style.display = "block";
-        scroll(0,0);
-}
-}
-
-// function to create our event listeners
-function createEventListeners(){
+// Function to create our event listeners
+function createEventListeners() {
+    // Event listeners for month select list
     var deliveryMonth = document.getElementById("delivMo");
     if (deliveryMonth.addEventListener) {
         deliveryMonth.addEventListener("change", updateDays, false);
-    } else if(deliveryMonth.attachEvent) {
+    } else if (deliveryMonth.attachEvent) {
         deliveryMonth.attachEvent("onchange", updateDays);
     }
-
+    // Event listener for years select list
     var deliveryYear = document.getElementById("delivYr");
     if (deliveryYear.addEventListener) {
         deliveryYear.addEventListener("change", updateDays, false);
     } else if (deliveryYear.attachEvent) {
         deliveryYear.attachEvent("onchange", updateDays);
     }
-
+    // Event listener for change in custom text field
     var messageBox = document.getElementById("customText");
     if (messageBox.addEventListener) {
         messageBox.addEventListener("change", autoCheckCustom, false);
     } else if (messageBox.attachEvent) {
         messageBox.attachEvent("onchange", autoCheckCustom);
     }
-
+    
+    // Event listener for checkbox to copy data 
     var same = document.getElementById("sameAddr");
     if (same.addEventListener) {
         same.addEventListener("change", copyBillingAddress, false);
     } else if (same.attachEvent) {
         same.attachEvent("onchange", copyBillingAddress);
     }
-
+    
+    // Event listener for validation on form submission
+    
     var form = document.getElementsByTagName("form")[0];
-   if (form.addEventListener) {
-       form.addEventListener("submit", validateForm, false);
-   } else if (form.attachEvent) {
-       form.attachEvent("onsubmit", validateForm);
-   }
+    if (form.addEventListener) {
+        form.addEventListener("submit", validateForm, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", validateForm);
+    }
 }
-
-
-// enable load event handlers
+// Enable load event handlers
 if (window.addEventListener) {
     window.addEventListener("load", setUpPage, false);
-} else if(window.attachEvent) {
+} else if (window.attachEvent) {
     window.attachEvent("onload", setUpPage);
 }
